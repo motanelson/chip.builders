1dfe5b5b152311605a11ff5a01f9d604ab194864
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-27 18:21:51 +0000

    Add files via upload

---
+++ packs.py
@@ -0,0 +1,18 @@
+
+print( "\033c\033[40;37m\n ? ")
+a="my.dat"
+num=[256*256*256,256*256,256,1]
+b=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+f1=open(a,"wb")
+for c in b:
+    remain=c
+    bbbb=""
+    for n in range(3):
+       i:int=remain//num[n]
+       ii=i*num[n]
+       bbb:chr=chr(i)
+       remain=remain-ii
+       bbbb=bbb+bbbb
+    bbbb=chr(remain)+bbbb
+    f1.write(bbbb.encode())
+f1.close()
